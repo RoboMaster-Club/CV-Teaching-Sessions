ee3f466a3253d08cd7dcda87447ab7398ded8f79
Lucas Rezac <lucas.m.rezac@gmail.com>
2019-04-06 16:09:59 +0000

    Fixed spacing on main_gpu.cpp

--- SOGA-Program/face-detection/main_gpu.cpp
+++ SOGA-Program/face-detection/main_gpu.cpp
@@ -1,16 +1,15 @@
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
 #include <iostream>
-
 using namespace std;
 using namespace cv;
-void detectAndDisplay( Mat frame, rs2::depth_frame & depth );
-Ptr<cuda::CascadeClassifier> face_cascade = cuda::CascadeClassifier::create(
-        "./haarcascade_frontalface_alt.xml");
-Ptr<cuda::CascadeClassifier> eyes_cascade = cuda::CascadeClassifier::create(
-        "./haarcascade_eye_tree_eyeglasses.xml");
-int main( int argc, const char** argv )
-{
+
+void detectAndDisplay(Mat frame, rs2::depth_frame& depth);
+
+Ptr<cuda::CascadeClassifier> face_cascade = cuda::CascadeClassifier::create("./haarcascade_frontalface_alt.xml");
+Ptr<cuda::CascadeClassifier> eyes_cascade = cuda::CascadeClassifier::create("./haarcascade_eye_tree_eyeglasses.xml");
+
+int main(int, const char**) {
     //-- 1. Read the video stream
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_COLOR, 848, 480, RS2_FORMAT_BGR8, 60);
@@ -28,14 +27,11 @@ int main( int argc, const char** argv )
     time_t startTime, endTime;
     unsigned long frameCount = 0;
     startTime = clock();
-    while ( frame.data )
-    {
+    while (frame.data) {
         //-- 2. Apply the classifier to the frame
         detectAndDisplay(frame, depth);
-        if( waitKey(1) == 27 )
-        {
+        if (waitKey(1) == 27)
             break; // escape
-        }
 	data = pipe.wait_for_frames();
 	color = data.get_color_frame();
 	depth = data.get_depth_frame();
@@ -51,7 +47,7 @@ int main( int argc, const char** argv )
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay(Mat frame, rs2::depth_frame & depth) {
+void detectAndDisplay(Mat frame, rs2::depth_frame& depth) {
     Size size = frame.size();
     int type = frame.type();
 
@@ -66,9 +62,8 @@ void detectAndDisplay(Mat frame, rs2::depth_frame & depth) {
     face_cascade->detectMultiScale(frame_gray, results_gpu);
     face_cascade->convert(results_gpu, faces);
 
-
     for (size_t i = 0; i < faces.size(); i++) {
-        Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+        Point center (faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
         ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
 	//calculate distance
 	float distance = depth.get_distance(center.x, center.y);
@@ -78,17 +73,15 @@ void detectAndDisplay(Mat frame, rs2::depth_frame & depth) {
 	cuda::GpuMat faceROI = frame_gray( faces[i] );
         //-- In each face, detect eyes
         std::vector<Rect> eyes;
-        eyes_cascade->detectMultiScale( faceROI, results_gpu );
+        eyes_cascade->detectMultiScale(faceROI, results_gpu);
 	eyes_cascade->convert(results_gpu, eyes);
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
+        for (size_t j = 0; j < eyes.size(); j++) {
+            Point eye_center (faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2);
+            int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
         }
     }
 
     //-- Show what you got
     imshow("Capture - Face detection", frame);
-
 }
