fd09794c6c4d2bcff4acc8be1d4ea93c3d6c8b4c
Lucas Rezac <lucas.m.rezac@gmail.com>
2019-04-06 16:06:55 +0000

    Fixed spacing on main_cpu.cpp

--- SOGA-Program/face-detection/main_cpu.cpp
+++ SOGA-Program/face-detection/main_cpu.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 using namespace std;
 using namespace cv;
-void detectAndDisplay( Mat frame, rs2::depth_frame & depth );
+
+void detectAndDisplay(Mat frame, rs2::depth_frame& depth);
+
 CascadeClassifier face_cascade("./haarcascade_frontalface_alt.xml");
 CascadeClassifier eyes_cascade("./haarcascade_eye_tree_eyeglasses.xml");
-int main( int argc, const char** argv )
-{
+
+int main(int, const char**) {
     //-- 1. Read the video stream
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_COLOR, 848, 480, RS2_FORMAT_BGR8, 60);
@@ -25,14 +27,11 @@ int main( int argc, const char** argv )
     time_t startTime, endTime;
     unsigned long frameCount = 0;
     startTime = clock();
-    while ( frame.data )
-    {
+    while (frame.data) {
         //-- 2. Apply the classifier to the frame
-        detectAndDisplay( frame, depth );
-        if( waitKey(1) == 27 )
-        {
+        detectAndDisplay(frame, depth);
+        if (waitKey(1) == 27)
             break; // escape
-        }
 	data = pipe.wait_for_frames();
 	color = data.get_color_frame();
 	depth = data.get_depth_frame();
@@ -46,33 +45,31 @@ int main( int argc, const char** argv )
     cfg.disable_stream(RS2_STREAM_COLOR);
     return 0;
 }
-void detectAndDisplay( Mat frame, rs2::depth_frame & depth )
-{
+
+void detectAndDisplay(Mat frame, rs2::depth_frame& depth) {
     Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
     //-- Detect faces
     std::vector<Rect> faces;
-    face_cascade.detectMultiScale( frame_gray, faces );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4 );
+    face_cascade.detectMultiScale(frame_gray, faces);
+    for (size_t i = 0; i < faces.size(); i++) {
+        Point center (faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+        ellipse(frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4);
 	float distance = depth.get_distance(center.x, center.y);
 	putText(frame, to_string(distance) + " m", Point(center.x, center.y),
-                        FONT_HERSHEY_SIMPLEX,
+     			FONT_HERSHEY_SIMPLEX,
                         1, Scalar(255, 0, 255), 2);
-        Mat faceROI = frame_gray( faces[i] );
+        Mat faceROI = frame_gray(faces[i]);
         //-- In each face, detect eyes
         std::vector<Rect> eyes;
-        eyes_cascade.detectMultiScale( faceROI, eyes );
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
+        eyes_cascade.detectMultiScale(faceROI, eyes);
+        for (size_t j = 0; j < eyes.size(); j++) {
+            Point eye_center (faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2);
+            int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
         }
     }
-    //-- Show what you got
-    imshow( "Capture - Face detection", frame );
+    //-- Show me what you got
+    imshow("Capture - Face detection", frame);
 }
